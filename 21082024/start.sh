8226dc424151dbd7cde24bc98fa60527e04a06ec
fabioalves291 <fabioalvesdeandrade29@gmail.com>
2024-08-27 01:07:26 -0300

    peqeunas alteracoes

--- 21082024/start.sh
+++ 21082024/start.sh
@@ -1,3 +1,3 @@
 docker build -t my-ubuntu-wordpress-image .
-docker run --name my-ubuntu-wordpress-container -p 8000:80 -d my-ubuntu-wordpress-image
+docker run --name my-ubuntu-wordpress-container -p 8000:80 -p 3306:3306 -d my-ubuntu-wordpress-image
 
