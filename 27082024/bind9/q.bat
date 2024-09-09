022c88b38c6e7b27bc445e68e22001041eb47ff4
fabioalves291 <fabioalvesdeandrade29@gmail.com>
2024-09-09 00:44:01 -0300

    add

--- 27082024/bind9/q.bat
+++
@@ -1,6 +0,0 @@
-docker rmi bind9
-docker build -t bind9 .
-docker stop bind9
-docker container rm bind9
-docker run -d --name bind9 -p 53:53/tcp -p 53:53/udp bind9
-
--- 27082024/nginx/cod.bat
+++
@@ -1,5 +0,0 @@
-docker stop proxy
-docker container rm proxy
-docker build -t proxy -f Dockerfile.proxy .
-docker network create -d bridge asa-net
-docker run -dp 80:80 --net=asa-net --name proxy proxy
